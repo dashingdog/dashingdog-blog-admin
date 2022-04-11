c5eac90c62091c3a050658aab8df35b470a18378
dashingdog <545520197@qq.com>
2022-04-11 20:09:49 +0800

    test: 测试rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,9 +2,9 @@
  # @Description:
  # @Date: 2022-04-11 15:31:12
 ###
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist $DEPLOY_USER@$DEPLOY_HOST:tmp/$TRAVIS_BUILD_ID
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/$TRAVIS_BUILD_ID
 
-ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no 'rm -rf /tmp/old.build && mv web/dashingdog-blog-admin/dist /tmp/old.build && mv tmp/$TRAVIS_BUILD_ID web/dashingdog-blog-admin/dist'
+ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no 'rm -rf /tmp/old.build && mv web/dashingdog-blog-admin/tests /tmp/old.build && mv tmp/$TRAVIS_BUILD_ID web/dashingdog-blog-admin/dist'
 echo 'deploy success'
 
 
