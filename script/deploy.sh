8cbf6877ad4cd881acb48713621e96ccdb10b3f5
dashingdog <545520197@qq.com>
2022-04-11 19:44:41 +0800

    feat: add rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,19 +1,13 @@
-npm install
 ###
  # @Description:
  # @Date: 2022-04-11 15:31:12
 ###
 
-git  reset --hard origin $TRAVIS_BRANCH
-
-git pull origin $TRAVIS_BRANCH
-
-echo 'update coude success, start install'
-
-npm install
-
-npm run build
-
+if [ ! -f "./dist" ]; then
+  echo "打包失败"
+  exit 1
+fi
+sftp 
 echo 'build success'
 
 
