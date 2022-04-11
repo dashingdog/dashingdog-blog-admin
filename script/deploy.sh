b06db36f843b5d313889bce14cc82472149c04f9
dashingdog <545520197@qq.com>
2022-04-11 16:15:11 +0800

    feat: ci优化

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,18 @@ npm install
  # @Description:
  # @Date: 2022-04-11 15:31:12
 ###
+cd ./web/dashingdog-blog-admin
+
+git  reset --hard origin $TRAVIS_BRANCH
+
+git pull origin $TRAVIS_BRANCH
+
+echo 'update coude success, start install'
+
+npm install
 
 npm run build
 
 echo 'success'
+
+
