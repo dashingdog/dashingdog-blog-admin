04e3ac741fe9e1a63bd601ada30b722f6940eed4
dashingdog <545520197@qq.com>
2022-04-12 10:40:42 +0800

    test: 测试rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,8 @@
  # @Date: 2022-04-11 15:31:12
 ###
 ls $TRAVIS_BUILD_DIR/tests
-rsync -av --delete-after   -e "ssh  -o PubkeyAuthentication=yes   -o stricthostkeychecking=no" $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/
-
+#rsync -av --delete-after   -e "ssh  -o PubkeyAuthentication=yes   -o stricthostkeychecking=no" $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/
+scp -o stricthostkeychecking=no -r $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/
 ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no 'rm -rf /tmp/old.build && mv web/dashingdog-blog-admin/tests /tmp/old.build && mv tmp/$TRAVIS_BUILD_ID web/dashingdog-blog-admin/dist'
 echo 'deploy success'
 
