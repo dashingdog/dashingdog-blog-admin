2d52352daa38a195f79936a56246068f211d0ff5
frankin <38437685+dashingdog@users.noreply.github.com>
2022-04-12 17:45:49 +0800

    feat: ci optimize  (#1)
    feat: ci optimize

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,19 +1,16 @@
-npm install
 ###
  # @Description:
  # @Date: 2022-04-11 15:31:12
 ###
+#rsync -av --delete-after   -e "ssh  -o PubkeyAuthentication=yes   -o stricthostkeychecking=no" $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/
+echo 'TRAVIS_BUILD_ID:' $TRAVIS_BUILD_ID
+ls $TRAVIS_BUILD_DIR/dist
+tar -zcf dist.tar.gz ./dist
+scp -o stricthostkeychecking=no -v dist.tar.gz $DEPLOY_USER@$DEPLOY_HOST:tmp
+echo 'scp upload success'
+ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no ' tar -zxvf tmp/dist.tar.gz -C tmp \
+  && rm -rf tmp/dist.tar.gz tmp/old.build && mv www/dist tmp/old.build && mv tmp/dist www/dist'
 
-git  reset --hard origin $TRAVIS_BRANCH
-
-git pull origin $TRAVIS_BRANCH
-
-echo 'update coude success, start install'
-
-npm install
-
-npm run build
-
-echo 'build success'
+echo 'deploy success'
 
 
