d759e35d0b60febcabb15927f3919eb6c3f03126
dashingdog <545520197@qq.com>
2022-04-12 15:17:31 +0800

    test: 测试rsync

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,9 +5,13 @@
 #rsync -av --delete-after   -e "ssh  -o PubkeyAuthentication=yes   -o stricthostkeychecking=no" $TRAVIS_BUILD_DIR/tests $DEPLOY_USER@$DEPLOY_HOST:tmp/
 echo 'TRAVIS_BUILD_ID:' $TRAVIS_BUILD_ID
 ls $TRAVIS_BUILD_DIR/dist
+tar -zcvf ${TRAVIS_BUILD_ID}_tar.gz $TRAVIS_BUILD_ID
 scp -o stricthostkeychecking=no -r $TRAVIS_BUILD_DIR/dist/ $DEPLOY_USER@$DEPLOY_HOST:tmp/$TRAVIS_BUILD_ID
 echo 'scp upload success'
-ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no 'rm -rf tmp/old.build && mv web/dashingdog-blog-admin/dist tmp/old.build && cp -r tmp/$TRAVIS_BUILD_ID/ web/dashingdog-blog-admin/dist'
+ssh "$DEPLOY_USER"@"$DEPLOY_HOST" -o StrictHostKeyChecking=no 'tar -zxvf ${TRAVIS_BUILD_ID}_tar.gz && rm-rf ${TRAVIS_BUILD_ID}_tar.gz  \
+ rm -rf tmp/old.build && mv web/dashingdog-blog-admin/dist tmp/old.build \
+ && cp -r tmp/$TRAVIS_BUILD_ID/ web/dashingdog-blog-admin/dist'
+
 echo 'deploy success'
 
 
